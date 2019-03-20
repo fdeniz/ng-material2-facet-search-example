83c252cf016323f74cc718b05ffd177073426059
fdeniz <itsfirat@gmail.com>
2019-03-20 14:59:02 -0400

    4 files modified

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# angular-xyprh9
+# Angular Facet Search/Filter Example
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xyprh9)
+This is an example application for demonstrating [ng-material2-facet-search](https://github.com/fdeniz/ng-material2-facet-search) project.
+
+[Try it](https://ng-material2-facet-search-example.stackblitz.io/) - ([Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-material2-facet-search-example))
+
+![](https://i.imgur.com/ZyS5SPk.png)
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NgMaterial2FacetSearchModule } from 'ng-material2-facet-search';
-import { MatCardModule, MatDividerModule } from '@angular/material';
-
+import { MatCardModule, MatDividerModule, MatCheckboxModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatDividerModule,
+    MatCheckboxModule,
     NgMaterial2FacetSearchModule
   ],
   declarations: [AppComponent],
